84b8d89a898edb05af734e2e80503200ee987400
Cloud User <2021wc86111@wilp.bits-pilani.ac.in>
2025-07-14 01:37:55 +0530

    Upload working Maven project with pom.xml

---
+++ src/main/java/com/ci/demo/App.java
@@ -0,0 +1,7 @@
+package com.ci.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins Continuous Integration!");
+    }
+}
---
+++ src/test/java/com/ci/demo/AppTest.java
@@ -0,0 +1,11 @@
+package com.ci.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
